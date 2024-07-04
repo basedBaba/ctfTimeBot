fe8df23d3ea0c5ded6e50d416c8542a08e4e6f0f
basedBaba <basedBaba@proton.me>
2024-07-04 20:45:02 +0530

    Add top teams command

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
+import json
 import os
+import requests
+from datetime import datetime
 from dotenv import load_dotenv
+from tabulate import tabulate
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -10,6 +14,29 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+base_url = "https://ctftime.org/api/v1"
+headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"}
+
+def top_teams():
+    endpoint = "/top/"
+    year = str(datetime.now().year)
+    table_headers = ["Place", "Team", "Rating"]
+
+    response = requests.get(base_url+endpoint, headers=headers)
+    top_teams = json.loads(response.content)
+
+    top_teams_list = []
+    rank = 1
+    for top_team in top_teams[year]:
+        team = [rank, top_team["team_name"], top_team["points"]]
+        top_teams_list.append(team)
+        rank += 1
+
+    reply = tabulate(top_teams_list, table_headers, tablefmt="rounded_outline", floatfmt=".2f")
+    reply = "```"+reply+"```"
+
+    return reply
 
 @client.event
 async def on_ready():
@@ -22,5 +49,9 @@ async def on_message(message):
     
     if message.content == PREFIX+"hello":
         await message.channel.send("Hello!")
+
+    if message.content == PREFIX+"top":
+        reply = top_teams()
+        await message.channel.send(reply)
 
 client.run(TOKEN)
